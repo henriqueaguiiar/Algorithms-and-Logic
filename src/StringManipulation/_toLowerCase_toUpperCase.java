0bf79c3fb2b674cc3e8039389e016fb12d3eedcf
Henrique Aguiar <henriqueaguiarpacheco09@gmail.com>
2025-02-24 17:27:33 -0300

    Add string methods toLowerCase and toUpperCase

---
+++ src/StringManipulation/_toLowerCase_toUpperCase.java
@@ -0,0 +1,22 @@
+package StringManipulation;
+
+public class _toLowerCase_toUpperCase {
+    public static void main(String[] args) {
+
+        // --- Metodo toLowerCase usado para deixar doa a string com caracteres minusculos -----
+
+        //toLowerCase, deixa a string toda em minusculo
+        String variavel1 = "HENRIQUE";
+        System.out.println(variavel1);
+        System.out.println("toLowerCase: " + variavel1.toLowerCase());
+
+        System.out.println("");
+
+        //toUpperCase deixa a string toda em maiusula
+        String variavel2 = "daniel";
+        System.out.println(variavel2);
+        System.out.println("toUpperCase: "+variavel2.toUpperCase());
+
+
+    }
+}
